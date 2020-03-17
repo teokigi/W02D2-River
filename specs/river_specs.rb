9956a1a214dda2c80da47e37769785bac9534d92
Tony Tang <teokigi@hotmail.com>
2020-03-17 20:09:35 +0000

    river, initialize and premade test data

--- specs/river_specs.rb
+++ specs/river_specs.rb
@@ -9,7 +9,20 @@ require_relative('../bears')
 class RiverTest < MiniTest::Test
 
     def setup
+        @fish1 = Fish.new "tuna"
+        @fish2 = Fish.new"salmmon"
+        @fish3 = Fish.new"mackarel"
 
+        @fishes = [@fish1,@fish2,@fish3]
+
+        @the_clyde = River.new("the Clyde",fishes)
+
+        @bear1 = Bears.new("Freddy",:Brown,[@Fish1,@fish2])
     end
+
+    #fish count
+    def test_001_
+
+    #remove fish, add to bear belly
 
 end
